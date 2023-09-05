f5cd360318c6e7ea5dd351569906d0ac679f73e9
tien1302 <tiennqse150168@fpt.edu.vn>
2023-09-05 22:34:45 +0700

    folder

---
+++ BAL/Authentications/PermissionAuthorizeAttribute.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Authentications
+{
+    public class PermissionAuthorizeAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        private string[] _roles;
+        public PermissionAuthorizeAttribute(params string[] roles)
+        {
+            this._roles = roles;
+        }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (context.HttpContext.User.Identity.IsAuthenticated)
+            {
+
+                var roleClaim = context.HttpContext.User.Claims.FirstOrDefault(x => x.Type.ToLower() == "role");
+                if (this._roles.FirstOrDefault(x => x.Trim().ToLower().Equals(roleClaim.Value.Trim().ToLower())) == null)
+                {
+                    context.Result = new ObjectResult("Forbidden") { StatusCode = 403, Value = "You are not allowed to access this function!" };
+                }
+                else
+                {
+                    return;
+                }
+            }
+            else
+            {
+                context.Result = new StatusCodeResult(401);
+            }
+        }
+    }
+}
