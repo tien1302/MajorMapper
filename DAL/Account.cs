044c58fd225012912596fedfad94e8518f02f9cd
Thangpx1892001 <thangpxse150138@fpt.edu.vn>
2023-10-08 16:57:13 +0700

    update database

---
+++ DAL/Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL;
+
+public partial class Account
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public string? Gender { get; set; }
+
+    public DateTime? DoB { get; set; }
+
+    public int Role { get; set; }
+
+    public string? Address { get; set; }
+
+    public int? Phone { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public int? Turn { get; set; }
+
+    public DateTime CreateDateTime { get; set; }
+
+    public virtual ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+
+    public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
+
+    public virtual Role RoleNavigation { get; set; } = null!;
+
+    public virtual ICollection<Slot> Slots { get; set; } = new List<Slot>();
+
+    public virtual ICollection<Test> Tests { get; set; } = new List<Test>();
+}
