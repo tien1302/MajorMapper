d53793dd60e52142d6b0964c59721794a6cb856b
Thangpx1892001 <thangpxse150138@fpt.edu.vn>
2023-10-13 21:47:30 +0700

    add CRUD: Feedback, ReviewTest, TestResult

---
+++ BAL/DAOs/Interfaces/IReviewTestDAO.cs
@@ -0,0 +1,17 @@
+﻿using BAL.DTOs.ReviewTests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.DAOs.Interfaces
+{
+    public interface IReviewTestDAO
+    {
+        public List<GetReviewTest> GetAll();
+        public GetReviewTest Get(int key);
+        public void Create(CreateReviewTest create);
+        public void Delete(int key);
+    }
+}
---
+++ WebAPI/Controllers/FeedbackController.cs
@@ -0,0 +1,110 @@
+﻿using BAL.DAOs.Implementations;
+using BAL.DAOs.Interfaces;
+using BAL.DTOs.Feedbacks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        private IFeedbackDAO _feedbackDAO;
+
+        public FeedbackController(IFeedbackDAO feedbackDAO)
+        {
+            _feedbackDAO = feedbackDAO;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                List<GetFeedback> list = _feedbackDAO.GetAll();
+                return Ok(new
+                {
+                    Data = list
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                GetFeedback feedback = _feedbackDAO.Get(id);
+                return Ok(new
+                {
+                    Data = feedback
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    Message = ex.Message
+                });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateFeedback create)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                _feedbackDAO.Create(create);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateFeedback update)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                _feedbackDAO.Update(id, update);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _feedbackDAO.Delete(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
