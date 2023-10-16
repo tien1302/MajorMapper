51888f0dbbd0824379feeec9d2ce092aca433288
tien1302 <tiennqse150168@fpt.edu.vn>
2023-10-16 22:23:11 +0700

    fix bug

---
+++ BAL/DTOs/TestResults/CreateTestResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.DTOs.TestResults
+{
+    public class CreateTestResult
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int TestId { get; set; }
+
+        public DateTime CreateDateTime { get; set; }
+    }
+}
