2a446d45159ba971a97493e4a26276f4cbee745b
Thangpx1892001 <thangpxse150138@fpt.edu.vn>
2023-11-06 20:49:28 +0700

    update db

--- DAL/Models/Question.cs
+++ DAL/Models/Question.cs
@@ -7,11 +7,15 @@ public partial class Question
 {
     public int Id { get; set; }
 
+    public string MethodName { get; set; } = null!;
+
     public int Type { get; set; }
+
+    public string Description { get; set; } = null!;
 
     public DateTime CreateDateTime { get; set; }
 
-    public string Description { get; set; } = null!;
+    public string Status { get; set; } = null!;
 
     public virtual ICollection<TestQuestion> TestQuestions { get; set; } = new List<TestQuestion>();
 }
--- DAL/Models/TestResult.cs
+++ DAL/Models/TestResult.cs
@@ -13,8 +13,6 @@ public partial class TestResult
 
     public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
 
-    public virtual ICollection<ReviewTest> ReviewTests { get; set; } = new List<ReviewTest>();
-
     public virtual ICollection<Score> Scores { get; set; } = new List<Score>();
 
     public virtual Test Test { get; set; } = null!;
